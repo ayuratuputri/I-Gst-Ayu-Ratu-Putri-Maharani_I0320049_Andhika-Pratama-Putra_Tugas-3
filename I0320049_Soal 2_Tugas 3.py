498b8b10d3d9de5facdfa4092748b81c67b6e43a
I Gusti Ayu Ratu Putri Maharani <ayuratuputrimaha@gmail.com>
2021-03-18 11:50:12 +0800

    soal 2 tugas 3

---
+++ I0320049_Soal 2_Tugas 3.py
@@ -0,0 +1,7 @@
+dict = {'Nama':'Ayu', 'Hobi1':'Memasak', 'Hobi2':'Berenang', 'Hobi3': 'Berkebun', 'Sosmed1': 'Instagram:ayuratuputri', 'Sosmed2': 'Line:ayuratuputri', 'Sosmed3': 'WA: 0812456348272', 'Lagu1': 'Blue jeans', 'Lagu2': 'Best part', 'Lagu3': 'Sampai jadi debu', 'Makanan1': 'Bakso', 'Makanan2': 'Mie ayam', 'Makanan3': 'Gado-gado'}
+dict['Hobi1'] = 'Jalan-jalan'
+dict['Sosmed1'] = 'Instagram:iniyuratt'
+del dict['Makanan1']
+del dict['Makanan2']
+dict['Hobi4'] = 'Olahraga'
+print(dict)
